45cc85d504794fa9d3d776c4ad36a758dcb05ebc
Juuso Piippo <158200846+JuJuz1@users.noreply.github.com>
2024-06-08 20:03:12 +0300

    Death if staying still for too long

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,8 @@
 extends CharacterBody2D
 class_name Player
 
+signal player_death
+
 ## Speed
 const SPEED : int = 8000
 ## Jump height and scale for the gravity
@@ -9,6 +11,11 @@ const JUMP_VELOCITY : int = -300
 const GRAVITY_SCALE : float = 0.8
 ## Constant gravity when gliding
 const GRAVITY_GLIDE : int = 3500
+
+## Time till death if staying still
+const time_death : float = 2.0
+## Variable for checking how long movement is stopped
+var stopped : float = 0.0
 
 @onready var animation : AnimatedSprite2D = $AnimatedSprite2D
 
@@ -21,6 +28,7 @@ var in_bush : bool = false
 func _ready():
 	#animation.flip_h = true
 	starting_position = self.global_position
+	player_death.connect(GameManager._on_player_death)
 	pass
 
 
@@ -28,23 +36,20 @@ func _ready():
 func _physics_process(delta):
 	if is_on_floor():
 		animation.play("run")
-		animation.rotation = 0
 	
 	# Jumping
 	if Input.is_action_just_pressed("interact") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		animation.stop()
-		animation.play("jump")    
+		animation.play("jump")
 		var tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 		tween.tween_property(animation, "scale", Vector2(1, 0.9), 0.05)
 		tween.tween_property(animation, "scale", Vector2(1, 1), 0.05)
-		animation.rotate(-PI / 20)
 		# To not confuse with gliding
 		get_viewport().set_input_as_handled()
 	
 	# Gliding
 	if Input.is_action_pressed("interact") and not is_on_floor() and velocity.y > 0:
-		animation.rotation = 0
 		animation.stop()
 		animation.play("glide")
 		#gliding = true
@@ -60,8 +65,14 @@ func _physics_process(delta):
 	
 	# Check if player is stuck
 	if velocity.x == 0:
+		stopped += delta
+		if stopped > time_death:
+			player_death.emit()
 		animation.stop()
+	else:
+		stopped = 0.0
 	
+	print(stopped)
 	# Constant movement to the right
 	velocity.x = SPEED * delta
 	
