9b0c6c7c2a86cd3f38e17920383d2ef619b73b69
Juuso Piippo <158200846+JuJuz1@users.noreply.github.com>
2024-06-09 18:57:05 +0300

    Different pickable items to work correctly

--- scripts/pickable.gd
+++ scripts/pickable.gd
@@ -4,12 +4,17 @@ extends Area2D
 ## Signal for player when picking up item
 signal picked_up
 
+## Name of the pickable item
+var _name : String = "null"
 
 func _ready():
-	pass
+	## Get the name of the sprite2D node to determine item "class"
+	## Took this approach to mainly avoid groups
+	_name = get_child(0).get_child(0).name
+	print(_name)
 
 
 func _on_body_entered(body):
 	if body is Player:
 		self.queue_free()
-		picked_up.emit(self.name)
+		picked_up.emit(self._name)
